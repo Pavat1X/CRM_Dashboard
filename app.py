eef4c5e23c9d215e084966ecf09f80fe5b6bedbe
Pavat1X <pavatpichetsin@outlook.com>
2023-07-17 15:09:45 +0700

    added some trial code

--- app.py
+++ app.py
@@ -9,4 +9,21 @@ upload = st.file_uploader("Upload a CSV file", type='csv')
 if upload is not None:
     data = pd.read_csv(upload)
 
+## removing duplicates
+emails = list(dict.fromkeys(data['Email Number']))
 
+## wrangling metrics
+
+metrics_list = ['requests', 'delivered', 'unique opens',
+'cumulative unique open rate', 'unique clicks', 'cumulative unique click rate','spam reports','unsubscribes',
+'conversion']
+
+selection = st.selectbox('select the email number that you want to view', emails)
+
+show_button = st.button('view data')
+
+## represent data - will add time series later after figuring out how to get the latest data from sendgrid
+
+if show_button:
+	serve_data = data.loc[selection, metrics_list]
+	print(serve_data)
